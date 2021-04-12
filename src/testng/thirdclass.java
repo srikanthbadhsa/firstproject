3e47473f2f49ceb1ecf2cc4f9e5f4d99064f6803
vinod <kumar.vind1221@gmail.com>
2021-04-12 13:55:31 +0530

    second commit

---
+++ src/testng/thirdclass.java
@@ -0,0 +1,16 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class SecondClass {
+	
+	@Test
+	public void secondClassMethod()
+	{
+		System.out.println("This is second class MeTHOD");
+		System.out.println("This is second class MeTHOD");
+
+	}
+	
+
+}
